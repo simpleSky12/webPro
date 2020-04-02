d02fa39c5e679c618a2d95e1f9f54e521ef61283
smipleSky12 <905781330@qq.com>
2020-04-02 09:45:22 +0800

    koa-body 协议解析

---
+++ corsBody.js
@@ -0,0 +1,24 @@
+const Koa = require('koa');
+const Router = require('koa-router');
+const koaBody = require('koa-body');
+const cors = require('@koa/cors');
+
+const app = new Koa();
+const router = new Router();
+
+router.post('/post', async (ctx) => {
+    let {body} = ctx.request;
+    console.log(body);
+    console.log(ctx.request);
+    ctx.body = {
+        ...body
+    }
+});
+
+app.use(koaBody());
+app.use(cors());
+
+app.use(router.routes())
+    .use(router.allowedMethods());
+
+app.listen(3000);
