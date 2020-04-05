d28b43b64bbae6a9ef2d9f2951df84572fce3e36
smipleSky12 <905781330@qq.com>
2020-04-05 17:39:40 +0800

    1登陆验证码的生成 2vuelidate验证方式

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,124 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="layui-container">
+      <form class="layui-form layui-form-pane" action="">
+        <div class="layui-form-item" :class="{'form-group--error': $v.name.$error}">
+          <label class="layui-form-label">用户名</label>
+          <div class="layui-input-inline">
+            <input type="text"
+                   placeholder="请输入用户名"
+                   v-model.trim="$v.name.$model"
+                   @input="setName($event.target.value)"
+                   class="layui-input">
+          </div>
+          <div class="error layui-form-mid" v-if="!$v.name.required">用户名不得为空</div>
+          <div class="error layui-form-mid" v-if="!$v.name.email">用户名输入格式错误</div>
+        </div>
+        <div class="layui-form-item">
+          <label class="layui-form-label">密码</label>
+          <div class="layui-input-inline">
+            <input type="password" v-model="password"
+                   placeholder="请输入密码"
+                   class="layui-input">
+          </div>
+        </div>
+        <div class="layui-form-item code">
+          <label class="layui-form-label">验证码</label>
+          <div class="layui-input-inline">
+            <input type="text" placeholder="请输入验证码"
+                   v-model="code"
+                   class="layui-input">
+          </div>
+          <div class="layui-form-mid" v-html="svg" @click="getCaptcha"></div>
+          <a href="javascript:;" @click="getCaptcha">看不清换一张</a>
+        </div>
+        <button type="button" class="layui-btn">点击登陆</button>
+        <a href="http://www.layui.com" class="getPassword">忘记密码？</a>
+      </form>
     </div>
-    <router-view/>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+import axios from 'axios'
+import { required, email } from 'vuelidate/lib/validators'
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      svg: '',
+      name: '',
+      password: '',
+      code: ''
+    }
+  },
+  validations: {
+    name: {
+      required,
+      email
+    },
+    password: {
+      required
+    },
+    code: {
+      required
+    }
+  },
+  mounted () {
+    this.getCaptcha()
+  },
+  methods: {
+    setName (value) {
+      this.name = value
+      this.$v.name.$touch()
+    },
+    getCaptcha () {
+      axios.get('http://localhost:3000/captcha').then(res => {
+        if (res.status === 200) {
+          const obj = res.data
+          if (obj.code === 200) {
+            this.svg = obj.data
+          }
+        }
+      })
+    }
+  }
 }
+</script>
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
+<style lang="scss" scoped>
+#app{
+  background-color: #f2f2f2;
+}
+.layui-container {
+  background-color: #fff;
+}
+input {
+  width: 190px;
+}
+.code {
+  display: flex;
+  align-items: center;
+  a{
+    &:hover {
       color: #42b983;
     }
+  }
+}
+.getPassword {
+  margin-left: 10px;
+  &:hover {
+    color: #42b983;
+  }
+}
+.error {
+  display: none;
+}
+.form-group--error {
+  .error {
+    color: #ff2130;
+    display: block;
   }
 }
 </style>
